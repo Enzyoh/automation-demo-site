136d4af9291c195371aab80e998c510870dc3be4
mndeni <melusimndeni392@gmail.com>
2021-09-22 18:27:32 +0200

    replaced import with react direct reference in App js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import './App.css';
 import Signin from './Signin';
-import React, { useState } from 'react'
+import React from 'react'
 import Profile from './Profile';
 
 const App = () =>{
-  const [isSignedIn, setIsSignedIn] = useState(false)
-  const [userType, setUserType] = useState('')
+  const [isSignedIn, setIsSignedIn] = React.useState(false)
+  const [userType, setUserType] =React.useState('')
 
   return (
     <div className="App">
       <div className="App-header">
-        <h2> Automation demo site</h2>
+        <h2> Automation demo site for mndeni</h2>
         {!isSignedIn && <Signin setIsSignedIn={setIsSignedIn} setUserType={setUserType} />}
         {isSignedIn && <Profile userType={userType} />}
       </div>
